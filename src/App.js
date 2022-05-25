ab2b6d837274ed63ae0fb58d4595c7a08cadc56d
Fernando Monarca <fernando.monarca@hotmail.com>
2022-05-25 16:32:57 -0300

    Tests

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ultimateqa.com"
           target="_blank"
           rel="noopener noreferrer"
+          data-testid="learn-link"
         >
           Learn React
         </a>
